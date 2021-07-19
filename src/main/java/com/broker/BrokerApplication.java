b9f4afe9c9f0ba307038213a6f77ca1424a33535
sunmc <didi@sunmc.local>
2021-07-19 10:15:23 +0800

    fix

--- src/main/java/com/broker/BrokerApplication.java
+++ src/main/java/com/broker/BrokerApplication.java
@@ -3,10 +3,28 @@ package com.broker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class BrokerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrokerApplication.class, args);
+
+//        SpringApplication.run(BrokerApplication.class, args);
+
+
+        int a, c, b;
+
+        b = 2;
+        c = 30;
+
+        a = b = c - 1;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
+
+
 }
